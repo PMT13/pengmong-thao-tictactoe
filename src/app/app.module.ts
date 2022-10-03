e949e6362e7d715143f6172c076561eccde3f7d7
Peng Mong <pengmnt@gmail.com>
2022-10-03 11:49:30 -0500

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TeamSelectionComponent } from './team-selection/team-selection.component';
+import {FormsModule} from "@angular/forms";
+import { BoxComponentComponent } from './box-component/box-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    TeamSelectionComponent,
+    BoxComponentComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/interfaces/IBox.ts
@@ -0,0 +1,5 @@
+export interface IBox{
+  id: string,
+  empty: boolean,
+  team: string
+}
